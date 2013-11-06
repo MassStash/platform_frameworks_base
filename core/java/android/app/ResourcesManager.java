ad62edae9733d9acd361991ac9e49b6375c08b36
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-06 11:39:59 +0000

    ResourcesManager: Fix theme insertion at boot
    This was lost during the 4.4 ActivityThread reshuffle, put it back
    
    Change-Id: I96eadf691973e9a2daa099b55816fc103d129be7

--- core/java/android/app/ResourcesManager.java
+++ core/java/android/app/ResourcesManager.java
@@ -176,6 +176,7 @@ public class ResourcesManager {
         //}
 
         AssetManager assets = new AssetManager();
+        assets.setThemeSupport(compatInfo.isThemeable);
         if (assets.addAssetPath(resDir) == 0) {
             return null;
         }
@@ -196,6 +197,18 @@ public class ResourcesManager {
         } else {
             config = getConfiguration();
         }
+
+        /* Attach theme information to the resulting AssetManager when appropriate. */
+        if (compatInfo.isThemeable && config != null) {
+            if (config.customTheme == null) {
+                config.customTheme = CustomTheme.getBootTheme();
+            }
+
+            if (!TextUtils.isEmpty(config.customTheme.getThemePackageName())) {
+                attachThemeAssets(assets, config.customTheme);
+            }
+        }
+
         r = new Resources(assets, dm, config, compatInfo, token);
         if (false) {
             Slog.i(TAG, "Created app resources " + resDir + " " + r + ": "
