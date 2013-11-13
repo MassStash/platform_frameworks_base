2cc27c81a497ccffccf8d925631c5e866f929ebd
D4rKn3sSyS <jesdga95@gmail.com>
2013-11-13 18:50:32 +0100

    Lockscreen: See through (1/2)
    Add the ability to see through lockscreen.
    Adapted to kitkat from jellybean by @BigBrother1984
    
    Signed-off-by: D4rKn3sSyS <jesdga95@gmail.com>
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS2:
    
    * Adding missing sem-colon.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Change-Id: I9057a6e302a5a824799844f27e8f64ff677a520a
    
    PS3:
    
    * Replace and remove undefined mContext.
    * Define the boolean enableScreenRotation inside new method.
    * Whitespaces clean up.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS4:
    
    * Restore mContext in setKeyguardParams().
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS5:
    
    * Fixes for setKeyguardParams() and maybeCreateKeyguardLocked() methods, kitkat changed some things.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS6:
    
    * Missing sem-colon.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS7: Fix all the things.
    
    * Effects doesn't vanish after first unlock.
    * Created condition in updateShowWallpaper() method that just return if see through is activated, he was restoring the wallpaper at second lock.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS8: Cosmetic changes.
    
    * Correct java style name to seeThrough() and place it above updateShowWallpaper().
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS9: Other cosmetic changes.
    
    * Better name to the see through boolean.
    * Removed a space nitpick.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS10:
    
    * We had two booleans for the same thing, ditch the one defined in one class only and take the global.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Change-Id: I06d952f85baf284961a24c8c9f90b29f4aef719a
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2013, ParanoidAndroid Project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -3356,6 +3357,19 @@ public final class Settings {
           * @hide
           */
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
+
+        /**
+         * Should the non-intrsive incall ui be used
+         *
+         * @hide
+         */
+        public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
 
         /**
          *  Enable statusbar double tap gesture on to put device to sleep
