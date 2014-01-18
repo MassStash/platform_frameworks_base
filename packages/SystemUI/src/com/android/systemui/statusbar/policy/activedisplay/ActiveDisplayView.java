3da782379c6368fe695475fcac8a6a9f3ffaf748
Gidi Kroon <GidiKroon@gmail.com>
2014-01-18 15:37:18 +0100

    Base: Disable proximity sensor when screen goes on in ActiveDisplay/PocketMode.
    Active Display (in Pocket Mode) currently only switches off the proximity sensor in some cases.
    If the user switches on the screen itself, the sensor is left running. This change
    stops the sensor as soon as the screen goes on. Active Display already started the sensor each
    time the screen goes off.
    
    PS2: In the mode that turns the screen off again when the device is pocketed,
    the sensor stays on until you leave ActiveDisplay.
    
    Change-Id: Id7afcd858f341ad929606d302c5741efa99a7c6f

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -755,6 +755,10 @@ public class ActiveDisplayView extends FrameLayout {
 
     private void onScreenTurnedOn() {
         cancelRedisplayTimer();
+        // Only keep watching for pocketing if woken up by pocket mode.
+        if (!mWakedByPocketMode) {
+            disableProximitySensor();
+        }
     }
 
     private void onScreenTurnedOff() {
