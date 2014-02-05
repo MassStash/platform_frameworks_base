6676325169c3bb5864b50d1ac8161e4b6849c53f
Jorge Ruesga <jorge@ruesga.com>
2014-02-06 00:41:27 +0100

    keyguard: fix delay lock screen sound
    Change-Id: Ia4cd2c856df1573856386e1597dcc0a9600d25a5
    JIRA: CYAN-3362
    Issue: https://jira.cyanogenmod.org/browse/CYAN-3362
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -658,7 +658,6 @@ public class KeyguardViewMediator {
 
         if (timeout <= 0) {
             // Lock now
-            mSuppressNextLockSound = true;
             doKeyguardLocked(null);
         } else {
             // Lock in the future
@@ -1035,8 +1034,6 @@ public class KeyguardViewMediator {
                         + sequence + ", mDelayedShowingSequence = " + mDelayedShowingSequence);
                 synchronized (KeyguardViewMediator.this) {
                     if (mDelayedShowingSequence == sequence) {
-                        // Don't play lockscreen SFX if the screen went off due to timeout.
-                        mSuppressNextLockSound = true;
                         doKeyguardLocked(null);
                     }
                 }
