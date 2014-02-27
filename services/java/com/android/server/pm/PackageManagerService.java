9d0d4cc0592ab2b99be45039047f18a8c8e59401
Andros Rosa <andros2011@gmail.com>
2014-02-26 23:43:37 -0400

    Revert "BootMessage: show more info in boot dialog"
    This reverts commit e0837e1a2979d6fdabe99ce6e1708c1ac3ae607f.
    
    Change-Id: I1b7f9f9f49436fbda2f097bfc8f1ccc57aacaa24
    
    Conflicts:
    	services/java/com/android/server/pm/PackageManagerService.java

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -3902,7 +3902,17 @@ public class PackageManagerService extends IPackageManager.Stub {
             ExecutorService executorService = Executors.newFixedThreadPool(sNThreads);
             final long start = System.currentTimeMillis();
             for (PackageParser.Package pkg : pkgs) {
-                final PackageParser.Package p = pkg;
+                if (!isFirstBoot()) {
+                    i++;
+                    try {
+                        ActivityManagerNative.getDefault().showBootMessage(
+                                mContext.getResources().getString(
+                                        com.android.internal.R.string.android_upgrading_apk,
+                                        i, pkgs.size()), true);
+                    } catch (RemoteException e) {
+                    }
+                }
+                PackageParser.Package p = pkg;
                 synchronized (mInstallLock) {
                     if (!p.mDidDexOpt) {
                         executorService.submit(new Runnable() {
